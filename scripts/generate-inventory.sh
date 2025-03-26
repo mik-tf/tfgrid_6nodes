85043fa4231b6dced270d9e5ffbae409fb1177ad
mik-tf <logismos@protonmail.ch>
2025-03-25 22:26:22 -0400

    update generate inventory

--- scripts/generate-inventory.sh
+++ scripts/generate-inventory.sh
@@ -13,44 +13,32 @@ command -v jq >/dev/null 2>&1 || {
     exit 1;
 }
 
-# Generate base inventory
+# Clear existing file and generate new inventory
 echo "Generating inventory from Terraform outputs..."
+echo "# Kubernetes Control Plane Nodes" > "$OUTPUT_FILE"
+echo "[kube_control]" >> "$OUTPUT_FILE"
+
+# Generate control plane nodes (node1, node2, node3)
 tofu -chdir="$DEPLOYMENT_DIR" show -json | jq -r '
   .values.outputs.wireguard_ips.value | 
-  to_entries | map(
-    "\(.key) ansible_host=\(.value) ansible_user=root"
-  ) | .[]
-' > "$OUTPUT_FILE"
-
-# Append control nodes
-echo -e "\n[control]" >> "$OUTPUT_FILE"
-tofu -chdir="$DEPLOYMENT_DIR" show -json | jq -r '
-  .values.outputs.wireguard_ips.value |
-  keys | map(select(. | test("node_[0-2]"))) | .[]
+  to_entries | map(select(.key | test("node_[0-2]"))) | 
+  .[] | "node\((.key | split("_")[1] | tonumber + 1)) ansible_host=\(.value) ansible_user=root"
 ' >> "$OUTPUT_FILE"
 
+# Add worker nodes section
+echo -e "\n# Kubernetes Worker Nodes" >> "$OUTPUT_FILE"
+echo "[kube_node]" >> "$OUTPUT_FILE"
 
-# Append worker nodes
-echo -e "\n[worker]" >> "$OUTPUT_FILE"
+# Generate worker nodes (node4, node5, node6)
 tofu -chdir="$DEPLOYMENT_DIR" show -json | jq -r '
-  .values.outputs.wireguard_ips.value |
-  keys | map(select(. | test("node_[3-5]"))) | .[]
+  .values.outputs.wireguard_ips.value | 
+  to_entries | map(select(.key | test("node_[3-5]"))) | 
+  .[] | "node\((.key | split("_")[1] | tonumber + 1)) ansible_host=\(.value) ansible_user=root"
 ' >> "$OUTPUT_FILE"
 
-# Add children groups
-echo -e "\n[kube_control:children]\ncontrol" >> "$OUTPUT_FILE"
-echo -e "\n[kube_node:children]\nworker" >> "$OUTPUT_FILE"
-
-# Rename nodes from node_0 to node1, node_1 to node2, etc.
-echo "Renaming nodes in inventory..."
-for i in {0..5}; do
-    new_num=$((i + 1))
-    sed -i "s/node_${i}/node${new_num}/g" "$OUTPUT_FILE"
-done
-
-# Add all:vars (Explicit Python Interpreter)
-echo -e "\n[all:vars]" >> "$OUTPUT_FILE"
+# Add global variables
+echo -e "\n# Global Variables" >> "$OUTPUT_FILE"
+echo "[all:vars]" >> "$OUTPUT_FILE"
 echo "ansible_python_interpreter=/usr/bin/python3.12" >> "$OUTPUT_FILE"
 
 echo "Inventory generated: $OUTPUT_FILE"
-
